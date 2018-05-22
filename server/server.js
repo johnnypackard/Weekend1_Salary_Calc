513d5263f84be974418c7498416e08833ea75bb8
Johnny Packard <packard81@me.com>
2018-05-22 14:23:00 -0500

    added express to weekend assignment

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use( express.static( 'server/public'));
+const port = 5000;
+app.listen(port, function() {
+    console.log(`Listening on port ${port}`);
+});
